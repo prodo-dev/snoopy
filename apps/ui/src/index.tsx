b4bc8e3ff5b3f46fb313a941cea742d7edf577c8
Jake Runzer <jakerunzer@gmail.com>
2019-04-17 15:04:03 +0100

    init commit

---
+++ apps/ui/src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const App = () => (
+  <div>
+    <h1>Hello, World!</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
