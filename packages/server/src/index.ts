eab754f1ff6b257ab72c5ce846d4bbf60b8f7ce0
Jake Runzer <jakerunzer@gmail.com>
2019-04-17 15:41:00 +0100

    Server: Setup server to render the ui

---
+++ packages/server/src/index.ts
@@ -0,0 +1,35 @@
+import * as Express from "express";
+import * as Bundler from "parcel-bundler";
+import * as path from "path";
+
+const clientDir = path.resolve(__dirname, "../../ui");
+const entryFile = path.resolve(clientDir, "./public/index.html");
+const outDir = path.resolve(clientDir, "dist");
+const outFile = path.resolve(outDir, "index.html");
+
+export const start = async (port: number = 3000) => {
+  const app = Express();
+
+  const options = {
+    outDir,
+    outFile,
+    watch: true,
+    minify: false,
+    scopeHoist: false,
+    hmr: true,
+    detailedReport: false,
+    bundleNodeModules: true,
+  };
+
+  const bundler = new Bundler(entryFile, options);
+  bundler.bundle();
+
+  app.use(bundler.middleware());
+  app.listen(3000);
+
+  // tslint:disable-next-line:no-console
+  console.log(`Starting server on port ${port}`);
+};
+
+// tslint:disable-next-line:no-console
+start().catch(console.error);
