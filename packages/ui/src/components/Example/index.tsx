ccd08027c26623959cbf9ba03fb291edf5695e44
Andreja <andreja.kogovsek1@gmail.com>
2019-04-26 11:01:59 +0100

    UI: Make the examples background look transparent

--- packages/ui/src/components/Example/index.tsx
+++ packages/ui/src/components/Example/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import styled from "styled-components";
+import background from "../../media/transparent_background.png";
 import {Example} from "../../models";
 import {margins, paddings} from "../../styles";
 
@@ -14,21 +15,30 @@ const StyledExample = styled.div`
   border-radius: 4px;
 `;
 
+const Container = styled.div`
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url(${background});
+  padding: ${paddings.medium};
+`;
+
 const JsxContainer = styled.div`
-  background-color: white;
-  color: black;
+  background-image: url(${background});
+  background-repeat: repeat;
 `;
 
 const Title = styled.div`
   font-size: ${props => props.theme.fontSizes.subtitle};
   margin-bottom: ${margins.small};
+  color: ${props => props.theme.colors.text};
 `;
 
 export default (props: Props) => (
   <StyledExample>
     <Title className="example-title">{props.example.name}</Title>
-    <JsxContainer className="example-contents">
-      {props.example.jsx}
-    </JsxContainer>
+    <Container>
+      <JsxContainer className="example-contents">
+        {props.example.jsx}
+      </JsxContainer>
+    </Container>
   </StyledExample>
 );
