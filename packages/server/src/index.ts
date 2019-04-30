8f6d5bce312719e5e4890c871f0bacd5c8637858
Tom Dawes <tomdawes91@gmail.com>
2019-04-30 17:18:29 +0100

    Reset app stuff (moved to separate PR)

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,14 +1,31 @@
 import {checkMatch} from "@prodo/snoopy-component-search";
+import * as Express from "express";
 import * as fs from "fs";
+import * as Bundler from "parcel-bundler";
 import * as path from "path";
-import createApp from "./app";
 
 const clientDir = path.resolve(__dirname, "../../ui");
+const entryFile = path.resolve(clientDir, "./public/index.html");
 const outDir = path.resolve(clientDir, "dist");
 const outFile = path.resolve(outDir, "index.html");
 
 export const start = async (port: number = 3000) => {
-  const app = createApp(outDir, outFile);
+  const app = Express();
+
+  const options = {
+    outDir,
+    outFile,
+    watch: true,
+    cache: false,
+    minify: false,
+    scopeHoist: false,
+    hmr: true,
+    detailedReport: false,
+    bundleNodeModules: true,
+  };
+
+  const bundler = new Bundler(entryFile, options);
+  bundler.addAssetType(".ts", require.resolve("./component-asset"));
 
   const componentsFile = path.resolve(clientDir, "src/components.ts");
   fs.watch(process.cwd(), {recursive: true}, (_, filename) => {
@@ -19,5 +36,6 @@ export const start = async (port: number = 3000) => {
   });
 
   process.stdout.write(`Starting server on port ${port}...\n`);
+  app.use(bundler.middleware());
   app.listen(3000);
 };
