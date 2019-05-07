bd0a7e54da300b797136d79d8d754e73a15ccfbf
Jake Runzer <jakerunzer@gmail.com>
2019-05-07 16:23:18 +0100

    Storybook: Remove `Component` and `Example` stories

--- packages/ui-storybook/__mocks__/componentsMock.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-export {React as UserReact, ReactDOM as UserReactDOM};
-
-export const components = [];
-
-export const themes = [];
