2a6abd2004175054d166394b1e194b845ad5c1d3
Jake Runzer <jakerunzer@gmail.com>
2019-05-07 18:05:10 +0100

    UI: export styled-components in componentsMock

--- packages/ui/__mocks__/componentsMock.ts
+++ packages/ui/__mocks__/componentsMock.ts
@@ -1,7 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as StyledComponents from "styled-components";
 
-export {React as UserReact, ReactDOM as UserReactDOM};
+export {React as UserReact, ReactDOM as UserReactDOM, StyledComponents};
 
 export const components = [];
 
