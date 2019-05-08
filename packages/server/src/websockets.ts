67f0b8c6302aed4563d309c1a51c95d84b392717
Tom Dawes <tomdawes91@gmail.com>
2019-05-08 10:55:46 +0100

    Add a test

--- packages/server/src/websockets.ts
+++ packages/server/src/websockets.ts
@@ -1,13 +1,8 @@
-import {WebSocketEvents} from "@prodo/snoopy-api";
 import * as http from "http";
 import * as WebSocket from "ws";
 
 export default (server: http.Server) => {
   const wss = new WebSocket.Server({server});
 
-  wss.on("connection", (ws: WebSocket) => {
-    setTimeout(() => {
-      ws.send(JSON.stringify({type: WebSocketEvents.OPEN_COMPONENT}));
-    }, 1000);
-  });
+  wss.on("connection", () => {});
 };
