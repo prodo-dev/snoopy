dd08a44745b773bdd75857eb3fb30cda810575b5
Andreja <andreja.kogovsek1@gmail.com>
2019-05-09 15:14:46 +0100

    UI: Use MemoryRouter and log redirects

--- packages/ui/src/App/context.tsx
+++ packages/ui/src/App/context.tsx
@@ -1,5 +1,13 @@
+import {
+  MemoryRouter as Router,
+  Route,
+  RouteComponentProps,
+} from "react-router-dom";
+import styled from "styled-components";
 import ErrorBoundary from "../components/ErrorBoundary";
+import backgroundImage from "../media/transparent_background.png";
 import {Component, Context, Example, Theme} from "../models";
+import {paddings} from "../styles";
 
 // tslint:disable-next-line:no-var-requires
 const userImport = require(process.env.PRODO_COMPONENTS_FILE!);
@@ -15,6 +23,44 @@ const themes: Theme[] = userImport.themes;
 
 export const context: Context = {components, themes};
 
+const StyledLog = styled.div`
+  padding-top: ${paddings.small};
+  color: #f8f8f2;
+  font-size: 11pt;
+`;
+
+const Container = styled.div`
+  width: min-content;
+`;
+
+const LogRoute = (
+  props: RouteComponentProps<{
+    link: string;
+  }>,
+) => (
+  <StyledLog>
+    This action would have redirected you to /{props.match.params.link}.
+  </StyledLog>
+);
+
+const DarkerJsxContainer = styled.div`
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.2),
+      rgba(255, 255, 255, 0.2)
+    ),
+    url(${backgroundImage}) repeat;
+  padding: ${paddings.medium};
+  width: fit-content;
+`;
+
+const JsxContainer = styled.div`
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.7),
+      rgba(255, 255, 255, 0.7)
+    ),
+    url(${backgroundImage}) repeat;
+`;
+
 export const renderExample = (
   example: Example,
   theme: Theme,
@@ -22,13 +68,23 @@ export const renderExample = (
 ) => {
   const UserComponent = () => (
     <ErrorBoundary>
-      {theme && ThemeProvider ? (
-        <ThemeProvider theme={theme as any}>
-          <>{example.jsx}</>
-        </ThemeProvider>
-      ) : (
-        <>{example.jsx}</>
-      )}
+      <Router>
+        <Container>
+          <DarkerJsxContainer>
+            <JsxContainer className="example-contents">
+              {theme && ThemeProvider ? (
+                <ThemeProvider theme={theme as any}>
+                  <>{example.jsx}</>
+                </ThemeProvider>
+              ) : (
+                <>{example.jsx}</>
+              )}
+            </JsxContainer>
+          </DarkerJsxContainer>
+          <Route path="/" exact component={() => <StyledLog />} />
+          <Route path="/:link+" exact component={LogRoute} />
+        </Container>
+      </Router>
     </ErrorBoundary>
   );
 
